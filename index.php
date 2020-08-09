329f63adb52c4f9e5c79916a02ecca80ca15f3a6
Adonis Kahila <adonis@MacBook-Air.local>
2020-08-09 12:56:13 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/index.css">
+
+    </head>
+
+    <body class>
+        <div class="nav">
+            <a href="login.php">Login/Register</a>
+            <a href="index.php" style="float: left;">Home/logo</a>
+            <!-- <input type="text" placeholder="search"> -->
+        </div>
+    </body>
+</html>
